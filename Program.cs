b3c7f1714dd4a9e6d2ad73bc9d56d02f94e6d2f6
AAmjad <Mian Sahib@DESKTOP-1HIT6VB>
2020-08-24 21:18:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton fromEmployee = new Singleton();
+            //fromEmployee.PrintDetails("From Employee instance");
+            //Singleton fromStudent = new Singleton();
+            //fromStudent.PrintDetails("From Student instance");
+            //Console.ReadLine();
+
+            string[] employeeName = new string[3];
+            employeeName[0] = "Mark";
+            employeeName[1] = "Matt";
+            employeeName[2] = "John";
+
+            string[][] jaggedArray = new string[3][];
+            jaggedArray[0] = new string[3];
+            jaggedArray[1] = new string[1];
+            jaggedArray[2] = new string[2];
+            jaggedArray[0][0] = "Bachelor";
+            jaggedArray[0][1] = "Master";
+            jaggedArray[0][2] = "Doctorate";
+
+            jaggedArray[1][0] = "Bachelors";
+
+            jaggedArray[2][0] = "Bachelorss";
+            jaggedArray[2][1] = "Masters";
+            
+            for(int i=0; i<jaggedArray.Length; i++)
+            {
+                string[] innerArray = jaggedArray[i];
+                Console.WriteLine(employeeName[i]);
+                Console.WriteLine("---------");
+                for(int j=0; j<innerArray.Length; j++)
+                {
+                    Console.WriteLine(innerArray[j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        public Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value" + counter);
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
